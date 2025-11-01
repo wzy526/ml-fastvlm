15d091fee523442da4a2c7a2b30091d25f954689
Zhuofan <xzf23@mails.tsinghua.edu.cn>
2025-11-01 17:13:47 +1100

    fix resume

--- llava/train/train_dat.py
+++ llava/train/train_dat.py
@@ -885,7 +885,7 @@ def train(attn_implementation=None):
                     **data_module)
 
     if list(pathlib.Path(training_args.output_dir).glob("checkpoint-*")):
-        trainer.train(resume_from_checkpoint=training_args.resume_from_checkpoint)
+        trainer.train(resume_from_checkpoint=True)
     else:
         trainer.train()
     trainer.save_state()
